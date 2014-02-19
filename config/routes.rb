f4b5506410bb09f7c7ee05b96a71d5c50c79e347
Bodhiswattwa Tewari <zubin_zorro@rediff.com>
2014-02-19 20:38:26 +0530

    Some refinements in User Registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 CollegeWebSite::Application.routes.draw do
+  get "session/new"
+  get "welcome/new"
+  get "users/new"
   get "sign_up" => "users#new", :as => "sign_up"
+  root "welcome#new"
   resources :users
+  resources :welcome
+  resources :session
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
